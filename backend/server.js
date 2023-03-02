5c489ca151e4dd753c8da127f2aa760c1e4eabc7
Brent <baumgab3@gmail.com>
2023-03-01 20:37:45 -0600

    added express

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const cors = require('cors')
+const express = require('express');
+
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello fron express");
+})
+
+
+app.listen('8081', () => {
+    console.log("server started on port 8081");
+});
