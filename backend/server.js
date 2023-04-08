fa077deff7d920bbba08ff341bb6f4743788ee09
Brent <baumgab3@gmail.com>
2023-04-08 17:28:20 -0500

    add back dropped changes from bad push

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,22 @@ const cors = require('cors')
 const express = require('express');
 const productCategoryRoutes = require('./routes/productCategoryRoutes');
 const productRoutes = require('./routes/productRoutes');
-
+const userRoutes = require('./routes/userRoutes');
+const sessionRoutes = require('./routes/sessionRoutes');
+const orderRoutes = require("./routes/orderRoutes");
+const addressRoutes = require("./routes/addressRoutes");
 
 const app = express();
 app.use(cors())
 app.use(express.json());
-// app.get("/", (req, res) => {
-//     res.send("Hello fron express.");
-// })
 
 // end points
 app.use("/product-category", productCategoryRoutes);
 app.use("/product", productRoutes);
+app.use("/", userRoutes);
+app.use("/", sessionRoutes);
+app.use("/orders", orderRoutes);
+app.use("/addresses", addressRoutes);
 
 app.listen('8081', () => {
     console.log("server started on port 8081");
